479040dd9015fa7d9212c467e9e8449f84b2c14d
DESKTOP-G4GQ3O4\kim sandberg <60923@gymnasium.ax>
2024-02-01 12:55:50 +0200

    fixed issues

--- db/tidrapportering.sql
+++ db/tidrapportering.sql
@@ -1,6 +1,6 @@
 /*
 SQLyog Community
-MySQL - 8.0.28 : Database - tidrapportering
+MySQL - 8.0.31 : Database - tidappdb
 *********************************************************************
 */
 
@@ -12,32 +12,47 @@ MySQL - 8.0.28 : Database - tidrapportering
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-CREATE DATABASE /*!32312 IF NOT EXISTS*/`tidrapportering` /*!40100 DEFAULT CHARACTER SET utf8 COLLATE utf8_swedish_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
-
 /*Table structure for table `aktiviteter` */
+
+DROP TABLE IF EXISTS `aktiviteter`;
 
 CREATE TABLE `aktiviteter` (
   `ID` int NOT NULL AUTO_INCREMENT,
-  `Namn` varchar(20) COLLATE utf8_swedish_ci NOT NULL,
-  PRIMARY KEY (`ID`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8_swedish_ci;
+  `namn` varchar(30) COLLATE utf8mb3_swedish_ci NOT NULL,
+  PRIMARY KEY (`ID`),
+  UNIQUE KEY `namn` (`namn`)
+) ENGINE=InnoDB AUTO_INCREMENT=230 DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_swedish_ci;
 
 /*Data for the table `aktiviteter` */
 
+insert  into `aktiviteter`(`ID`,`namn`) values 
+(4,'fel'),
+(3,'health up'),
+(2,'javascript'),
+(1,'php');
+
 /*Table structure for table `uppgifter` */
+
+DROP TABLE IF EXISTS `uppgifter`;
 
 CREATE TABLE `uppgifter` (
   `ID` int NOT NULL AUTO_INCREMENT,
-  `Datum` date NOT NULL,
-  `Tid` time NOT NULL,
-  `Beskrivning` varchar(100) COLLATE utf8_swedish_ci DEFAULT NULL,
-  `AktivitetID` int NOT NULL,
+  `beskrivning` varchar(100) COLLATE utf8mb3_swedish_ci DEFAULT NULL,
+  `aktivitetid` int DEFAULT NULL,
+  `tid` time DEFAULT NULL,
+  `datum` date DEFAULT NULL,
   PRIMARY KEY (`ID`),
-  KEY `AktivitetID` (`AktivitetID`),
-  CONSTRAINT `uppgifter_ibfk_1` FOREIGN KEY (`AktivitetID`) REFERENCES `aktiviteter` (`ID`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8_swedish_ci;
+  KEY `aktivitetid` (`aktivitetid`),
+  CONSTRAINT `uppgifter_ibfk_1` FOREIGN KEY (`aktivitetid`) REFERENCES `aktiviteter` (`ID`)
+) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_swedish_ci;
 
 /*Data for the table `uppgifter` */
+
+insert  into `uppgifter`(`ID`,`beskrivning`,`aktivitetid`,`tid`,`datum`) values 
+(1,'yippiee',3,'22:00:00','2024-01-11'),
+(2,'woopsiee',2,'23:00:00','2024-01-06'),
+(3,'happie',3,'00:00:00','2024-01-22'),
+(4,'qippie',1,'01:00:00','2023-12-27');
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
 /*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
